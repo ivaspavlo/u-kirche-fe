674ec54209d907902def020c80a3297928918b61
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-12-05 08:24:31 +0100

    UK-14: Integrat i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 import { PRIME_NG_GLOBAL_MODULES } from './constants/primeng';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, ...PRIME_NG_GLOBAL_MODULES],
+    imports: [...PRIME_NG_GLOBAL_MODULES, RouterOutlet, TranslatePipe],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor(private translate: TranslateService, public primeNGConfig: PrimeNGConfig) {
+        this.translate.addLangs(['ua', 'en']);
+        this.translate.setDefaultLang('en');
+
+        const browserLang = translate.getBrowserLang();
+        const lang = browserLang?.match(/en|ua/) ? browserLang : 'en';
+
+        this.translate.use(lang);
+
+        this.translate.stream('primeng').subscribe(data => {
+            this.primeNGConfig.setTranslation(data);
+        });
+    }
+
+    public useLanguage(value: string): void {
+        this.translate.use(value);
+    }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
@@ -8,11 +8,23 @@ import { provideEffects } from '@ngrx/effects';
 import { CORE_ROUTES } from './app.routes';
 import { CORE_PROVIDERS } from './services/providers';
 import { PRIME_NG_GLOBAL_SERVICES } from './constants/primeng';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader =
+    (http: HttpClient) => new TranslateHttpLoader(http, '../assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
     providers: [
         ...CORE_PROVIDERS,
         ...PRIME_NG_GLOBAL_SERVICES,
+        importProvidersFrom([TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useFactory: httpLoaderFactory,
+              deps: [HttpClient]
+            },
+        })]),
         provideAnimations(),
         provideRouter(CORE_ROUTES, withComponentInputBinding()),
         provideHttpClient(withInterceptorsFromDi()),
