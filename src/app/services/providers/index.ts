35ada214e19bbde1d3d3c77d377694aa13e524f6
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-12-20 08:35:37 +0100

    UK-21: Adjust captcha implementation

--- src/app/services/providers/index.ts
+++ src/app/services/providers/index.ts
@@ -2,10 +2,12 @@ import { windowProvider } from './window.provider';
 import { localStorageProvider } from './local-storage.provider';
 import { localeProvider } from './locale.provider';
 import { AuthInterceptorProvider } from './auth-interceptor.provider';
+import { siteKeyProvider } from './site-key.provider';
 
 export * from './window.provider';
 export * from './local-storage.provider';
 export * from './locale.provider';
 export * from './auth-interceptor.provider';
+export * from './site-key.provider';
 
-export const CORE_PROVIDERS = [windowProvider, localStorageProvider, localeProvider, AuthInterceptorProvider];
+export const CORE_PROVIDERS = [windowProvider, localStorageProvider, localeProvider, AuthInterceptorProvider, siteKeyProvider];
