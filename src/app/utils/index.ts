078e1b9a11aa39c10d67bd637b057e51f4c1b9d4
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-01-15 08:32:10 +0100

    UK-32.1: Fix formatting

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,12 +1,9 @@
 export const get = (obj: object, path: string, defaultValue?: any): any => {
-  const result = String.prototype.split.call(path, /[,[\].]+?/)
-    .filter(Boolean)
-    .reduce((res, key) => {
-      return res !== null && res !== undefined
-        ? res[key as keyof object]
-        : res;
-    }, obj);
-  return result === undefined || result === obj
-    ? defaultValue
-    : result;
-}
+    const result = String.prototype.split
+        .call(path, /[,[\].]+?/)
+        .filter(Boolean)
+        .reduce((res, key) => {
+            return res !== null && res !== undefined ? res[key as keyof object] : res;
+        }, obj);
+    return result === undefined || result === obj ? defaultValue : result;
+};
