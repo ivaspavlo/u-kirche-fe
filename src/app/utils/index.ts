df0fb12b6ff7fb2abe872d78ea38154155ebe719
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-01-15 08:30:21 +0100

    UK-32: Add get util

---
+++ src/app/utils/index.ts
@@ -0,0 +1,12 @@
+export const get = (obj: object, path: string, defaultValue?: any): any => {
+  const result = String.prototype.split.call(path, /[,[\].]+?/)
+    .filter(Boolean)
+    .reduce((res, key) => {
+      return res !== null && res !== undefined
+        ? res[key as keyof object]
+        : res;
+    }, obj);
+  return result === undefined || result === obj
+    ? defaultValue
+    : result;
+}
